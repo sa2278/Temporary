892b9487557cec996c4c8c13c388a169cdb74469
sa2278 <160508951+sa2278@users.noreply.github.com>
2024-03-08 12:07:41 +0000

    Add files via upload

---
+++ ENG1_prototype-master/src/EventConstants.java
@@ -0,0 +1,21 @@
+public final class EventConstants {
+    private EventConstants(){
+
+    }
+
+    /*these are placeholder event times to be referred to, since they take
+    the same amount of time to do regardless a constant is used
+    this represents the time consumed for 3 events and eating. since sleeping always ends the
+    day it is not a constant value*/
+    public static final int eventATime = 1;
+    public static final int eventBTime = 1;
+    public static final int eventCTime = 1;
+    public static final int eventDTime = 1;
+
+
+    public static final int eventAEnergy = 1;
+    public static final int eventBEnergy = 1;
+    public static final int eventCEnergy = 1;
+    public static final int eventDEnergy = -1;
+
+}
